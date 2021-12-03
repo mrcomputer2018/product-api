31ca1084973843a804429374759b38ffb3bf371a
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2021-12-03 14:41:53 -0300

    insercao de dados em product

---
+++ src/main/resources/db/migration/V4__insert_products.sql
@@ -0,0 +1,7 @@
+
+
+
+INSERT INTO products.product(product_identifier, nome, preco, descricao, category_id) VALUES('carro-controle-remoto', 'Carrinho de Controle Remoto', 100.0, 'Um carrinho de controle remoto', 3);
+INSERT INTO products.product(product_identifier, nome, preco, descricao, category_id) VALUES('video-game', 'Video Game', 2000.0, 'Um video game', 1);
+INSERT INTO products.product(product_identifier, nome, preco, descricao, category_id) VALUES('tv', 'tv', 1000.0, 'Uma televisão', 1);
+
